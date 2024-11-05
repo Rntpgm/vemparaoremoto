96f490d13c374d4c409aaa3dca5dcd0834270371
Rntpgm <renato_renan1@hotmail.com>
2024-11-05 16:07:56 -0300

    cod loop

---
+++ test.py
@@ -0,0 +1,12 @@
+
+contador = 0
+while True:
+    nome = input("qual e seu nome? ")
+
+    if nome == "Renato":
+        print("Sim! este e o nome certo")
+        break
+    else:
+        contador += 1  # Incrementa o contador
+        print("Digite novamente")
+print(f"Voce digitou {contador} vezes.")
